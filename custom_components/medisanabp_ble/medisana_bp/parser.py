bf505a7ef1e8c8a3bf999cb1c7981e53be43be93
Vasilis Koulis <bkbilly@hotmail.com>
2025-03-04 18:33:20 +0200

    Update parser.py

--- custom_components/medisanabp_ble/medisana_bp/parser.py
+++ custom_components/medisanabp_ble/medisana_bp/parser.py
@@ -85,7 +85,6 @@ class MedisanaBPBluetoothDeviceData(BluetoothData):
             )
         except:
             _LOGGER.error("Can't add Measured Date")
-            return
 
         _LOGGER.info(
             "Got data from BPM device (syst: %s, diast: %s, puls: %s)",
